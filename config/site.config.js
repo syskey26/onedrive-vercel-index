30faafb3a1604e0d62065105c08cf892a7c66df1
syskey26 <37918820+syskey26@users.noreply.github.com>
2023-01-31 17:24:42 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,24 +38,19 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://github.com/spencerwooo/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>. Made with ❤ by SpencerWoo.',
+    '“Life is short, you need Python”',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
   protectedRoutes: ['/Private'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'admin#yubico.ml',
+  email: 'mailto:admin@yubico.ml',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
-  links: [
-    {
-      name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
-    },
-  ],
+  links: [],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
   // https://day.js.org/docs/en/display/format for detailed specification. The default value is ISO 8601 full datetime
